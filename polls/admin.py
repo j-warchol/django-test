8eebf96dee5068ec452163ad020da9afa514773d
Janek Warchoł <lemniskata.bernoullego@gmail.com>
2014-02-05 12:20:28 +0100

    better admin interface for polls and choices

--- polls/admin.py
+++ polls/admin.py
@@ -1,4 +1,18 @@
 from django.contrib import admin
-from polls.models import Poll
+from polls.models import Poll, Choice
 
-admin.site.register(Poll)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 1
+
+class PollAdmin(admin.ModelAdmin):
+    list_display = ('question', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question']
+    fieldsets = [
+        (None,               {'fields': ['question']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Poll, PollAdmin)
